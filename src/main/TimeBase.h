f596594d0d98edf4c8142b6c8a5167ee0d0bfce1
andreas <andreas@wellenvogel.de>
2018-12-08 17:52:04 +0100

    add debug speed up time

--- src/main/TimeBase.h
+++ src/main/TimeBase.h
@@ -1,6 +1,8 @@
 #ifndef TIMEBASE_H
 #define TIMEBASE_H
 #include <Arduino.h>
+//a number of bits to shift the time to speed up
+#define DEBUG_SPEED_UP 3 
 //32 bit seconds - about 136 years
 class TimeBase{
   private:
@@ -9,6 +11,7 @@ class TimeBase{
   public:
   static unsigned long timeSeconds(){
     unsigned long current=millis();
+    current >> DEBUG_SPEED_UP;
     if (current < lastMillis){
       overflowCounter++;
     }
@@ -16,11 +19,11 @@ class TimeBase{
     //we make some simple arithmetics here
     //and consider a second to be 1024 ms
     //for exact timings this needs to be handled outside
-    current=(current >> 10) | (overflowCounter << 24);
+    current=(current >> (10 - DEBUG_SPEED_UP)) | (overflowCounter << (24-DEBUG_SPEED_UP));
     return current;
   }
   static unsigned long initial(){
-    return (unsigned long)1 << 24; //initial value of overflow counter
+    return (unsigned long)1 << (24-DEBUG_SPEED_UP); //initial value of overflow counter
   }
   static unsigned long secondsSinceStart(){
     return timeSeconds()-initial(); 
