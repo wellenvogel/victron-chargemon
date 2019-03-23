c24d127d69678ecce0931426f7b728dbd1651d86
andreas <andreas@wellenvogel.de>
2019-03-23 20:28:24 +0100

    add selection of display interval 6/12/all

--- gui/src/ChartsView.js
+++ gui/src/ChartsView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ToolBar from './components/ToolBar';
 import Button from 'react-toolbox/lib/button';
+import { RadioGroup, RadioButton } from 'react-toolbox/lib/radio';
 import { ComposedChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Bar, Cell, ReferenceLine } from 'recharts';
 import Measure from 'react-measure';
 import Helper from './components/Helper.js';
@@ -12,7 +13,8 @@ const SETTINGSURL=BASEURL+'set';
 const DEFAULT_WIDTH=300;
 const DEFAULT_HEIGHT=300;
 
-
+const DISPLAY_6H=6*3600;
+const DISPLAY_12H=12*3600;
 
 const formatDateToText=Helper.formatDateToText;
 const findFromDataArray=Helper.findFromDataArray;
@@ -21,13 +23,14 @@ class ChartsView extends Component {
 
     constructor(props){
         super(props);
-        this.state={running:true,width:DEFAULT_WIDTH,height: DEFAULT_HEIGHT};
+        this.state={running:true,width:DEFAULT_WIDTH,height: DEFAULT_HEIGHT,runtime:0, displayInterval:0};
         this.goBack=this.goBack.bind(this);
         this.fetchData=this.fetchData.bind(this);
         this.setError=this.setError.bind(this);
         this.decodeData=this.decodeData.bind(this);
         this.resizeChart=this.resizeChart.bind(this);
         this.fetchSettings=this.fetchSettings.bind(this);
+        this.handleDisplayInterval=this.handleDisplayInterval.bind(this);
 
     }
     setError(err){
@@ -81,10 +84,10 @@ class ChartsView extends Component {
         let values=[];
         let min=999999;
         let max=0;
+        let start=new Date();
         for (let i in data){
             let item=data[i];
             if (item.definition.name == 'TE'){
-                let start=new Date();
                 let idx=0;
                 for (let pidx=item.value.length-1;pidx >=0;pidx--){
                     let itemValues = item.value[pidx].split(',');
@@ -106,6 +109,7 @@ class ChartsView extends Component {
                 };
             }
         }
+        let runtime=0;
         if (values.length > 0){
             //set x-axis labels
             for (let i=0;i<values.length;i++){
@@ -121,8 +125,9 @@ class ChartsView extends Component {
                 let length=findFromDataArray(data,'TS',true);
                 if (length) storeData.percent=(sum*100)/length;
             }
+            runtime=start.getTime()/1000-values[0].date.getTime()/1000;
         }
-        this.setState({data:storeData,running:false,error:undefined});
+        this.setState({data:storeData,running:false,error:undefined,runtime:runtime});
     }
     componentDidMount(){
         this.fetchData();
@@ -132,6 +137,9 @@ class ChartsView extends Component {
         if (this.state.width != rect.entry.width || this.state.height != rect.entry.height){
             this.setState({ width:rect.entry.width,height:rect.entry.height });
         }
+    }
+    handleDisplayInterval(nval){
+        this.setState({displayInterval:nval})
     }
 
     render() {
@@ -163,6 +171,21 @@ class ChartsView extends Component {
                 );
             }
             return null;
+        };
+
+        const SelectTime=function(props){
+            if (props.runtime < DISPLAY_6H) return null;
+            return (
+                <div className="displayInterval">
+                    <RadioGroup className="displayIntervalInner" name='displayInterval' value={props.displayInterval}
+                                onChange={self.handleDisplayInterval}>
+                        <RadioButton label='all' value={0}/>
+                        {(props.runtime > DISPLAY_12H) ? <RadioButton label='12h' value={DISPLAY_12H}/> : null}
+                        {(props.runtime > DISPLAY_6H) ? <RadioButton label='6h' value={DISPLAY_6H}/> : null}
+                    </RadioGroup>
+                </div>
+            );
+
         };
 
         const Chart = function (props) {
@@ -205,11 +228,13 @@ class ChartsView extends Component {
         };
         let Summary= function(props){
             if (! props.sum) return null;
+
             return (
                 <div className="summary">
                     <div className="summaryFrom">
                         <span className="label">Since</span>
                         <span className="value">{props.values[0]?formatDateToText(props.values[0].date,true):''}</span>
+                        <span className="value secondValue">({Helper.secondsToTime(props.runtime)})</span>
                     </div>
                     <div className="summaryOn">
                         <span className="label">On</span>
@@ -222,6 +247,22 @@ class ChartsView extends Component {
                 </div>
             );
         };
+        let filteredValues=[];
+        if (this.state.data && this.state.data.values) {
+            let now = new Date();
+            if (this.state.displayInterval == 0) {
+                filteredValues = this.state.data.values;
+            }
+            else {
+                let start = new Date();
+                start.setTime(now.getTime() - this.state.displayInterval * 1000);
+                for (let i in this.state.data.values) {
+                    if (this.state.data.values[i].date >= start) {
+                        filteredValues.push(this.state.data.values[i]);
+                    }
+                }
+            }
+        }
         return (
             <div className="view chartsView">
                 <ToolBar >
@@ -236,8 +277,9 @@ class ChartsView extends Component {
                         <Error data={this.state.error}/>:
                         this.state.data.values?
                             <div className="chartWrapper">
-                            <Chart values={this.state.data.values}/>
-                            <Summary {...this.state.data}/>
+                            <SelectTime runtime={this.state.runtime} displayInterval={this.state.displayInterval}/>
+                            <Chart values={filteredValues}/>
+                            <Summary {...this.state.data} runtime={this.state.runtime}/>
                             </div>
                             :null
                 }
